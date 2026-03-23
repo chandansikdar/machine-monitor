9adbe10cc0d5f691e3013153bc96fd6b312e3ab9
chandansikdar <chandan@symbion.ch>
2026-03-23 21:09:22 +0100

    Update main.py

--- main.py
+++ main.py
@@ -677,7 +677,7 @@ if "last_multi_results" not in st.session_state:
 # ------------------------------------------------------------------ #
 
 @st.cache_resource
-def get_services(_version=2):
+def get_services(_version=3):
     return Database(), Visualizer()
 
 db, viz = get_services()
